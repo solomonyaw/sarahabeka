9e126ebf26073cb5c23915acdb2544923d441877
solomonyaw <sadeklo@st.vvu.eu.gh>
2023-01-17 03:59:46 -0800

    added files

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,4 @@ spinner = document.querySelector("#spinner");
 if (!sign) { angle = angle + 45; } else { angle = angle - 45; }
 spinner.setAttribute("style","-webkit-transform: rotateY("+ angle +"deg); -moz-transform: rotateY("+ angle +"deg); transform: rotateY("+ angle +"deg);");
 }
+
